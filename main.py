d89d1ceaeb5c98e916984e0bc0dfa38c640425e9
Lapua <c011731950@edu.teu.ac.jp>
2020-03-21 15:24:49 +0900

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import tasks
+
+TOKEN = ''
+CHANNEL_ID = '690796724901642265'
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+    channel = client.get_channel(CHANNEL_ID)
+    print(channel)
